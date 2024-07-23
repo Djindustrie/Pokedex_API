7ccdb10f7a6b0e8fb390ad6164603956ae59b5cb
Djindustrie <Djindustrie@Protonmail.com>
2024-07-23 10:52:48 +0200

    The search function runs with ender

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 const POKEAPI_URL = "https://pokeapi.co/api/v2/";
 
-function onloadFunc() {
-  loadData();
-}
-
 async function loadData(path = "pokemon/") {
   let response = await fetch(POKEAPI_URL + path);
   let responseToJson = await response.json();
   console.log(responseToJson);
 }
 
-// Die such Function 
-async function searchPokemonName() {
+// Die such Function
+
+function searchPokemonName(event) {
+  if (event.key === "Enter") {
+    const input = event.target.value;
+    pokemonName(input);
+    event.target.vakue = "";
+  }
+}
+
+async function pokemonName(input) {
   try {
-    const pokemonName = document
-      .getElementById("pokemonName")
-      .value.toLowerCase();
+    const pokemonName = input.toLowerCase();
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
     );
     if (!response.ok) {
       throw new Error("Could not fetch resource");
     }
-    pokemonSprite(response);
+    searchPokemonSprite(response);
   } catch (error) {
     console.error(error);
   }
 }
 
-async function pokemonSprite(response) {
+async function searchPokemonSprite(response) {
   try {
     const data = await response.json();
     const pokemonSprite = data.sprites.front_default;
@@ -38,4 +41,8 @@ async function pokemonSprite(response) {
   } catch (error) {
     console.error(error);
   }
+}
+
+function onloadFunc() {
+  loadData();
 }
