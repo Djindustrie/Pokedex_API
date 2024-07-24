ffd52747385e154f847465f0e37552050051fd64
Djindustrie <Djindustrie@Protonmail.com>
2024-07-24 23:27:18 +0200

    Code clean

--- script.js
+++ script.js
@@ -1,93 +1,75 @@
-// Die such Function
+let url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0.";
+let pokemonArray = [];
+let limit = 20;
 
-function searchPokemonName(event) {
-  if (event.key === "Enter") {
-    const input = event.target.value;
-    pokemonName(input);
-    event.target.vakue = "";
-  }
-}
+// function getTypeColor(type) {}
 
-async function pokemonName(input) {
-  try {
-    const pokemonName = input.toLowerCase();
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    );
-    notResponse(response);
-    searchPokemonSprite(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
 
-function notResponse(response) {
-  if (!response.ok) {
-    throw new Error("Could not fetch resource");
-  }
-}
-
-async function searchPokemonSprite(response) {
-  try {
-    const data = await response.json();
-    const pokemonSprite = data.sprites.front_default;
-    const imgElement = document.getElementById("pokemonSprite"); // Die ID muss noch an das Passende HTML Element geändert werden.
-    imgElement.src = pokemonSprite;
-    imgElement.style.display = "block";
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Content
-
-async function loadData() {
-  let url = `https://pokeapi.co/api/v2/pokemon/`;
+async function getContent(){
   let response = await fetch(url);
-  let responseToJson = await response.json();
-  let results = responseToJson["results"];
-  renderPokemonCart(results);
+  let responseAsJson = await response.json();
+  let results = responseAsJson["results"]
+  pokemonDataGrab(results);
+}
+
+async function pokemonDataGrab(results){
+  for (let i = 0; i < limit; i++) {
+    const result = results[i];
+    let pokemonData = await fetch(result.url);
+    let pokemonJson = await pokemonData.json();
+    let pokemonSprite = pokemonJson.sprites.front_default;
+
+    let pokemonTypes = pokemonJson.types.map((typeInfo) =>
+      capitalizeFirstLetter(typeInfo.type.name)
+    );
+
+    let pokemonName = capitalizeFirstLetter(result["name"]);
+    let typesHtml = "";
+    pokemonTypes.forEach((type) => {
+      typesHtml += `<div class="pokemonCardUnderTyp typFont ${type}">${type}</div>`;
+    });
+
+    let speciesData = await fetch(pokemonJson.species.url);
+    let speciesJson = await speciesData.json();
+    let pokedexNumber = speciesJson.id;
+
+    pokemonArray.push({
+      name: pokemonName,
+      sprite: pokemonSprite,
+      types: pokemonTypes,
+      pokedexNumber: pokedexNumber,
+      typesHtml: typesHtml
+    });
+  }
+  renderPokemonCart();
 }
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-async function renderPokemonCart(results) {
+function renderPokemonCart(){
   let content = document.getElementById("pokemonCard");
-  content.innerHTML = '';
-  for (let i = 0; i < 20; i++) {
-    const result = results[i];
-
-    let pokemonData = await fetch(result.url);
-    let pokemonJson = await pokemonData.json();
-    let pokemonSprite = pokemonJson.sprites.front_default;
-    let pokemonTypes = pokemonJson.types.map(typeInfo => capitalizeFirstLetter(typeInfo.type.name));
-    let pokemonName = capitalizeFirstLetter(result['name']);
-    let typesHtml = '';
-    pokemonTypes.forEach(type => {
-      typesHtml += `<div class="pokemonCardUnderTyp fontNormel">${type}</div>`;
-    });
+  content.innerHTML = "";
+  for (let i = 0; i < pokemonArray.length; i++) {
+    const pokemon = pokemonArray[i];
 
     content.innerHTML += /*HTML*/ `
-      <div class="pokemonCard">
-        <div>
-          <h2 class="h2">${pokemonName}</h2>
+          <div onclick="toggleOverlay()" class="pokemonCard ${pokemon['types'][0]}">
+        <div class="pokemonCardUpPart">
+        <h2 class="pokedexNumberFont"># ${pokemon['pokedexNumber']}</h2>
+          <h2 class="h2">${pokemon['name']}</h2>
         </div>
         <div class="pokemonCardUnderPart">
-          <div id="pokemonCardUnderTyp">
-          ${typesHtml}
+          <div>
+          ${pokemon['typesHtml']}
           </div>
-          <img id="pokemonSprite${i}" src="${pokemonSprite}" alt="Pokemon Sprite">
+          <img id="pokemonSprite${i}" src="${pokemon['sprite']}" alt="Pokemon Sprite">
         </div>
         <div></div>
       </div>
     `;
+
+    console.log(pokemon);
   }
-}
-
-// Aufruf der loadData Funktion
-
-function onloadFunc() {
-  loadData();
 }
